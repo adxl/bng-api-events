5f5ed7b35180b8e7022562353ef6b4c61590bef6
Adel Sen <adelsenhadjii@gmail.com>
2023-06-21 21:52:10 +0200

    Feat: add events seeds factory

--- src/fixtures/seeds/events-winners/events-winners.data.ts
+++ src/fixtures/seeds/events-winners/events-winners.data.ts
@@ -1,47 +1,36 @@
 import { DeepPartial } from 'typeorm';
 import { EventWinner } from '../../../domains/events-winners/events-winners.entity';
 
-export const eventsWinners: DeepPartial<EventWinner>[] = [
-  {
-    event: {
-      id: '1a6bf203-06e5-4e9e-aa76-d7e12eba4a01',
-    },
-    userId: 'c63a4bd1-cabd-44ee-b911-9ee2533dd014',
-    rank: 1,
-  },
-  {
-    event: {
-      id: '1a6bf203-06e5-4e9e-aa76-d7e12eba4a01',
-    },
-    userId: 'c63a4bd1-cabd-44ee-b911-9ee2533dd017',
-    rank: 2,
-  },
-  {
-    event: {
-      id: '1a6bf203-06e5-4e9e-aa76-d7e12eba4a01',
-    },
-    userId: 'c63a4bd1-cabd-44ee-b911-9ee2533dd020',
-    rank: 3,
-  },
-  {
-    event: {
-      id: '1a6bf203-06e5-4e9e-aa76-d7e12eba4a02',
-    },
-    userId: 'c63a4bd1-cabd-44ee-b911-9ee2533dd025',
-    rank: 1,
-  },
-  {
-    event: {
-      id: '1a6bf203-06e5-4e9e-aa76-d7e12eba4a02',
-    },
-    userId: 'c63a4bd1-cabd-44ee-b911-9ee2533dd016',
-    rank: 2,
-  },
-  {
-    event: {
-      id: '1a6bf203-06e5-4e9e-aa76-d7e12eba4a02',
-    },
-    userId: 'c63a4bd1-cabd-44ee-b911-9ee2533dd014',
-    rank: 3,
-  },
+const userWins = [
+  [16, 17, 18],
+  [16, 20, 18],
+  [16, 14, 17],
+  [17, 16, 19],
+  [19, 18, 15],
+  [16, 22, 17],
+  [17, 14, 16],
+  [16, 18, 17],
+  [22, 14, 16],
+  [16, 19, 18],
 ];
+
+export const eventsWinners: DeepPartial<EventWinner>[] = userWins.flatMap((ranks, index) => [
+  {
+    id: `88888888-06e5-4e9e-aa76-d7e12eba4a${String(index * 3).padStart(2, '0')}`,
+    event: { id: `77777777-06e5-4e9e-aa76-d7e12eba4a${String(index + 1).padStart(2, '0')}` },
+    userId: `c63a4bd1-cabd-44ee-b911-9ee2533dd0${String(ranks[0])}`,
+    rank: 1,
+  },
+  {
+    id: `88888888-06e5-4e9e-aa76-d7e12eba4a${String(index * 3 + 1).padStart(2, '0')}`,
+    event: { id: `77777777-06e5-4e9e-aa76-d7e12eba4a${String(index + 1).padStart(2, '0')}` },
+    userId: `c63a4bd1-cabd-44ee-b911-9ee2533dd0${String(ranks[1])}`,
+    rank: 2,
+  },
+  {
+    id: `88888888-06e5-4e9e-aa76-d7e12eba4a${String(index * 3 + 2).padStart(2, '0')}`,
+    event: { id: `77777777-06e5-4e9e-aa76-d7e12eba4a${String(index + 1).padStart(2, '0')}` },
+    userId: `c63a4bd1-cabd-44ee-b911-9ee2533dd0${String(ranks[2])}`,
+    rank: 3,
+  },
+]);
--- src/fixtures/seeds/events/events.data.ts
+++ src/fixtures/seeds/events/events.data.ts
@@ -1,26 +1,14 @@
 import { Event } from 'src/domains/events/events.entity';
 import { DeepPartial } from 'typeorm';
 
-export const events: DeepPartial<Event>[] = [
-  {
-    id: '1a6bf203-06e5-4e9e-aa76-d7e12eba4a01',
-    name: 'Event 1',
-    startsAt: '2023-05-05T13:46:53+0000',
-    endedAt: '2023-05-08T13:46:53+0000',
-    stationId: '11111111-bab3-439d-965d-0522568b0005',
-  },
-  {
-    id: '1a6bf203-06e5-4e9e-aa76-d7e12eba4a02',
-    name: 'Event 2',
-    startsAt: '2023-06-10T13:46:53+0000',
-    endedAt: '2023-06-15T13:46:53+0000',
-    stationId: '11111111-bab3-439d-965d-0522568b0003',
-  },
-  {
-    id: '1a6bf203-06e5-4e9e-aa76-d7e12eba4a03',
-    name: 'Event 3',
-    startsAt: '2023-06-20T13:46:53+0000',
-    endedAt: null,
-    stationId: '11111111-bab3-439d-965d-0522568b0001',
-  },
-];
+const stationsMap = [1, 3, 4, 5, 2, 2, 1, 8, 6, 1, 2];
+
+export const events: DeepPartial<Event>[] = Array(11)
+  .fill(null)
+  .map((_, index) => ({
+    id: `77777777-06e5-4e9e-aa76-d7e12eba4a${String(index + 1).padStart(2, '0')}`,
+    name: `Event ${index + 1}`,
+    startsAt: `2022-${String(index + 1).padStart(2, '0')}-01T09:00`,
+    endedAt: index !== 10 ? `2022-${String(index + 1).padStart(2, '0')}-04T12:00` : null,
+    stationId: `11111111-bab3-439d-965d-0522568b000${stationsMap[index]}`,
+  }));
